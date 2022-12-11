9140fb4bd0cd9bc1b54011ab5702ad4aac7b84e5
Imanrmd29 <iman.ramadhan@students.utdi.ac.id>
2022-12-11 19:40:01 +0700

    Menambahakan Unit Test AddViewMOdel

---
+++ app/src/test/java/com/iman/story_akhir1/add/AddViewModelTest.kt
@@ -0,0 +1,67 @@
+package com.iman.story_akhir1.add
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.iman.story_akhir1.DataDummy
+import com.iman.story_akhir1.MainDispatcherRule
+import com.iman.story_akhir1.core.DataRepo
+import com.iman.story_akhir1.core.Resource
+import com.iman.story_akhir1.core.data.remote.model.GeneralRespon
+import com.iman.story_akhir1.ui.add.AddViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class AddViewModelTest{
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Mock
+    private lateinit var dataRepository: DataRepo
+
+    private lateinit var addViewModel: AddViewModel
+    private val generalResponseMock = DataDummy.generalResponse()
+    private val fileMock = DataDummy.multipartFile()
+    private val tokenMock = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLXRrenRtVi1tQ0hHRDYyOEQiLCJpYXQiOjE2NjcxMTcyMTZ9.sR3fwAxHj64b5aGa7KfuhlGaQzw-zUx2hn8t_kjt0W0"
+    private val descriptionMock = "bukan description"
+    private val latitudeMock = -6.857053F
+    private val longitudeMock = 107.53229F
+
+
+    @Before
+    fun setUp() {
+        addViewModel = AddViewModel(dataRepository)
+        addViewModel.setStoryParam(tokenMock, fileMock, descriptionMock, latitudeMock, longitudeMock)
+    }
+
+    @Test
+    fun `when add story`()  = runTest {
+        val expectedResponse : Flow<Resource<GeneralRespon>> = flow {
+            emit(Resource.Success(generalResponseMock))
+        }
+        Mockito.`when`(dataRepository.addNewStory(tokenMock, fileMock, descriptionMock, latitudeMock, longitudeMock)).thenReturn(
+            expectedResponse
+        )
+        addViewModel.postStory.observeForever {
+            verify(dataRepository).addNewStory(tokenMock, fileMock, descriptionMock, latitudeMock, longitudeMock)
+            assertNotNull(it.data)
+            assertTrue(it is Resource.Success)
+            assertFalse(it is Resource.Error)
+            assertFalse(it?.data?.error ?: false)
+        }
+    }
+}
